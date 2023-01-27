105a9ec56ac29a6e76694d2511e0d67787c5f279
Agustin Martinez <agustinmar.1993@gmail.com>
2023-01-27 13:46:54 -0300

    fix

--- script/script.js
+++ script/script.js
@@ -1,18 +1,11 @@
 let nombreDeUsuario = prompt("Para sacar turno ingrese un nuevo nombre de usuario")
-let contraseñaNueva = prompt("Ahora ingrese una contraseña númerica")
-contraseñaNueva = typeof Number
+let contraseñaNueva = Number(prompt("Ahora ingrese una contraseña númerica"))
 
-while(contraseñaNueva == typeof Object){
+
+while(isNaN(contraseñaNueva) || contraseñaNueva === 0){
     alert("La contraseña debe ser númerica")
-    contraseñaNueva = prompt("ahora ingrese una contraseña numerica")
+    contraseñaNueva = Number(prompt("ahora ingrese una contraseña numerica"))
 }
-
-
-//do {
-//    if (contraseñaNueva != Number)
-//        alert("ingrese una contraseña númerica")
-//        contraseñaNueva = prompt ("Ahora ingrese una contraseña númerica")
-//} while (contraseñaNueva != Number);
 
 
 for(let turno = 1; turno <= 10; turno++){
