790accea85eb4182960847501c14b6b544957848
Agustin Martinez <agustinmar.1993@gmail.com>
2023-02-24 15:02:09 -0300

    terminando cosas

--- script/main.js
+++ script/main.js
@@ -1,9 +1,6 @@
 const categorias = ['CLINICA', 'FECHA', 'HORA'];
-
 const opcionesTurno = [];
-
 const turnos = [];
-
 
 class selectTurno {
     constructor(id, nombre, categoria) {
@@ -35,7 +32,6 @@ opcionesTurno.push(new selectTurno(10, '19:00 Hs', categorias[2]));
 opcionesTurno.push(new selectTurno(11, '19:30 Hs', categorias[2]));
 opcionesTurno.push(new selectTurno(12, '20:00 Hs', categorias[2]));
 
-// Funciones para obtener datos del form
 
 function obtenerNombre() {
     let obtenerN = document.getElementById('ingresoNombre').value;
@@ -47,33 +43,24 @@ function obtenerDNI() {
     return obtenerC;
 }
 
-// Toastify nombre
-
 function toastiNombre() {
-
     let nombreToasti = document.getElementById('ingresoNombre').value;
-
     Toastify({
         text: `Bienvenido/a ${nombreToasti}!`,
         duration: 3000,
         gravity: 'top',
         position: 'right',
         style: {
-
             background: "linear-gradient(to right, #9b8b3e, #088200)",
         },
     }).showToast();
-
 }
 
 
 let nombreForm = document.getElementById('submitNombre');
-
 nombreForm.onclick = () => {
     toastiNombre();
 }
-
-// Funcion con método
 
 function obtenerDato(id) {
     let dato = document.getElementById(id).value;
@@ -90,30 +77,19 @@ function obtenerTurno() {
 function crearTurno() {
     let turnn = new Turno(turnos.length , obtenerNombre(), obtenerDNI(), obtenerTurno())
     turnos.push(turnn);
-
     let pedidoStorage = JSON.parse(localStorage.getItem('comandas')) || [];
-
     pedidoStorage.push(turnn);
-
     let pedidoStorageJSON = JSON.stringify(pedidoStorage);
-
     localStorage.setItem('comandas', pedidoStorageJSON) || [];
-
-
-
-
     let listaOrden = "";
     turnn.orden.forEach(item => {
         listaOrden += `<li>${item.nombre}</li>`;
     });
-
-
     let divReporte = document.getElementById('pedir')
     divReporte.innerHTML = `<div>DATOS DE SU TURNO: <ul>${listaOrden}</ul></div>`
 }
 
 let pedidoForm = document.getElementById("pedirComanda");
-
 pedidoForm.onclick = () => {
     crearTurno();
     Toastify({
@@ -125,18 +101,14 @@ pedidoForm.onclick = () => {
             background: "linear-gradient(to right, #9b8b3e, #088200)",
         },
     }).showToast();
-
 }
 
 function recogerDatos() {
     let DNI = document.getElementById('ingresoDNI').value;
-
     let cubArray = JSON.parse(localStorage.getItem("personaDNI")) || [];
     cubArray.push(DNI);
     let cubArrayJSON = JSON.stringify(cubArray);
-
     localStorage.setItem("personaDNI", cubArrayJSON) || [];
-
 
     Toastify({
         text: `DNI Ingresado : ${DNI}`,
@@ -144,18 +116,15 @@ function recogerDatos() {
         gravity: 'top',
         position: 'right',
         style: {
-
             background: "linear-gradient(to right, #9b8b3e, #088200)",
         },
     }).showToast();
 
     let reporteTitulo = document.getElementById('titulo')
     reporteTitulo.innerHTML = `<h3>DNI N°: ${DNI}</h3>`
-
 }
 
 let dniForm = document.getElementById("submitDNI");
-
 dniForm.onclick = () => {
     recogerDatos()
 }
